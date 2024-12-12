71a43792e678503be95d5a91319954ff6cd0bf78
JiangRS17 <92614825+JiangRS17@users.noreply.github.com>
2024-12-12 11:32:35 +0800

    Add files via upload

---
+++ bridge_12.12/transmission/include/trans_manager.h
@@ -0,0 +1,15 @@
+#ifndef TRANS_MANAGER_H
+#define TRANS_MANAGER_H
+
+#include <stdint.h>
+#include "../../manager/include/bridge_manager.h"
+
+
+int32_t ReadBridgeDeviceTable(const BridgeInfo *info,int requestId);
+
+// int32_t BridgeHandle();
+
+
+
+
+#endif
